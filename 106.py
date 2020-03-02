bc62a713b1f747f1cb2bca06b77ee8f07271de36
Andressa Theodoro <38594220+Andressatheodoro@users.noreply.github.com>
2020-03-02 14:22:44 -0300

    Add files via upload

---
+++ 106.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 18:58:45 2020
+
+@author: andressa theodoro
+"""
+
+'''
+aula de  programação pra internet resolução de esercícios:
+'''
+
+
+
+###
+## Exercicios
+###
+
+'''
+     1) Implemente o metodo define_default_city de acordo com a docstring 
+    definida no inicio da funcao. Utilize a clausula else no loop implementado.
+'''
+'''
+    2) Remova do arquivo capitais-BR.csv todas capitais dos estados do sudeste
+    e teste se sua funcao estah robusta o suficiente.
+'''
+'''
+     3) Faca uma funcao que le o arquivo lista-cpf.txt, retorne a quantidade de
+     CPF unicos (sem repeticao) e os escreva em um arquivo lista-cpf-unicos.txt.
+     Eh necessario descompactar o arquivo lista-cpf.txt.tar.gz primeiro.
+'''
